0595ca00d0c1b8d0436b282822faf10a7c72cdc5
Pavel Isaev <isaevpd4@gmail.com>
2016-02-22 00:43:11 +0200

    p26

--- Solutions/p26.py
+++ Solutions/p26.py
@@ -1,7 +1,28 @@
+def divide(n):
+    divide_by = list()
+    start = 1
+    while True:
+        if start == 0:
+            break
+        if start < n:
+            start *= 10 
+        else:
+            if start in divide_by:
+                break
+            divide_by.append(start)
+            #print 'dividing {}/{} remainder is {}'.format(start, n, start % n)
+            start = start % n
+
+    return len(divide_by)
 
 
+my_max = float('-inf')
+result = -1
 
+for n in range(2, 1000):
+    temp = divide(n)
+    if temp > my_max:
+        my_max = temp
+        result = n
 
-# print Fraction(16, 64) * Fraction(38, 76) * Fraction(27, 54) * Fraction(49, 98)
-
- 
+print "d is equal to {} and the length of the cycle is {}".format(result, my_max)
